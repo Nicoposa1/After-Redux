5c87b44ab44e9c98969b527ab8f2bda63a9b40c1
Nicolas Posa <nicolasposa@Nicolass-Air.home>
2022-03-04 19:25:03 -0300

    adding redux

--- components/List/index.js
+++ components/List/index.js
@@ -1,15 +1,31 @@
-import { StyleSheet, FlatList } from "react-native";
-import React from "react";
-import ListItem from "./ListItem";
+import React from 'react';
+import {
+  StyleSheet,
+  FlatList,
+} from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItem } from '../../store/actions/items.action';
+import ListItem from './ListItem';
 
-const List = ({ itemList, handleModal }) => {
+const List = () => {
+  const dispatch = useDispatch();
+  const itemList = useSelector(state => state.items.list);
+
+  const handleModal = (id) => dispatch(removeItem(id));
+  
   return (
     <FlatList
       data={itemList}
-      renderItem={(data) => <ListItem data={data} handleModal={handleModal} />}
-      keyExtractor={(item) => item.id}
+      renderItem={(data) => <ListItem handleModal={handleModal} data={data} />}
+      keyExtractor={item => item.id}
     />
   );
-};
+}
+
+const styles = StyleSheet.create({
+  items: {
+    marginTop: 20,
+  },
+});
 
 export default List;
